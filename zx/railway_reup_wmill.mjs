4621496484799d8535015bd16afd342cd1d70823
rconjoe <admin@trog.host>
2024-01-17 08:23:41 -0800

    update script

--- zx/railway_reup_wmill.mjs
+++ zx/railway_reup_wmill.mjs
@@ -1,1 +1,18 @@
-await $`railway whoami`;
+cd("~/github.com/diamondfoundry/diamantaire");
+await spinner("reupping wmill services...", async () => {
+  const services = [
+    "server",
+    "workers: default",
+    "workers: native",
+    "workers: reports",
+    "lsp",
+  ];
+  try {
+    const res = Promise.all(services.map((s) => $`railway up --service ${s}`));
+    res.forEach((r) => console.log(r.toString()));
+    console.log(chalk.bgGreen.black(`${s} :: SUCCESS `));
+  } catch (e) {
+    console.log(e.toString());
+    process.exitCode = 1;
+  }
+});
