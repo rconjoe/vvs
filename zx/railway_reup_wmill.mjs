0bccaf8b0fe3e4572a373b92e149c9755d579af6
Joe <joe@rconjoe.com>
2024-01-17 08:31:31 -0800

    Update railway_reup_wmill.mjs

--- zx/railway_reup_wmill.mjs
+++ zx/railway_reup_wmill.mjs
@@ -1,4 +1,4 @@
-cd("~/github.com/diamondfoundry/diamantaire");
+cd("/home/trog/github.com/diamondfoundry/diamantaire");
 await spinner("reupping wmill services...", async () => {
   const services = [
     "server",
