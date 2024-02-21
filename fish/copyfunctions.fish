3cd9efc35e609f1c3ed43e082bf72c51370bcf02
trog <admin@trog.host>
2024-02-20 18:09:03 -0800

    fish: init copy fish functions script

---
+++ fish/copyfunctions.fish
@@ -0,0 +1,5 @@
+#!/usr/bin/env fish
+
+mv /home/trog/vvs/fish/functions/ /home/trog/vvs/fish/functions.bak/
+cp -r /home/trog/.config/fish/functions/ /home/trog/vvs/fish/functions/
+rm -rf /home/trog/vvs/fish/functions.bak/
